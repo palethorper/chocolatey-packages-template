8feee05608da1f67d50f0f97edbace608da24d68
Chocolatey <chocolatey@realdimensions.net>
2017-07-07 00:06:15 +0000

    AU: 1 updated - metricbeat [skip ci] https://gist.github.com/13606e0925d63bcef190c34da28a87a7/baf767933b0b6032cd79a376dbb2c976e456457c

--- automatic/metricbeat/tools/chocolateyinstall.ps1
+++ automatic/metricbeat/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
 $packageName= 'metricbeat'
 
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.4.0-windows-x86.zip'
-$url64      = 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.4.0-windows-x86_64.zip'
+$url        = 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.5.0-windows-x86.zip'
+$url64      = 'https://artifacts.elastic.co/downloads/beats/metricbeat/metricbeat-5.5.0-windows-x86_64.zip'
 
 $installationPath = $toolsDir
 
@@ -13,9 +13,9 @@ $packageArgs = @{
   unzipLocation = $installationPath
   url           = $url
   url64bit      = $url64
-  checksum      = '6244da5b6af7bcf9a43e293067e2037cecddc712'
+  checksum      = '0c4f903f0eb44ab048532febdc24fd4af856cb14'
   checksumType  = 'sha1'
-  checksum64    = 'd864a4a3c554731022d096ad97c60c0f3ea887d9'
+  checksum64    = '53e16c529b9edddbf0ab87e44a0f07d34f7bfbda'
   checksumType64= 'sha1'
 }
 
