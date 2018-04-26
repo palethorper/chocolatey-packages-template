60ceb6418c0f5dbb132e313c3bbbc8f6d311f082
Andrew M. Stoltz <astoltz@questarai.com>
2018-04-26 16:11:07 -0500

    Support uninstall/upgrade

--- automatic/winlogbeat/tools/chocolateyinstall.ps1
+++ automatic/winlogbeat/tools/chocolateyinstall.ps1
@@ -8,6 +8,25 @@ $url64      = 'https://artifacts.elastic.co/downloads/beats/winlogbeat/winlogbea
 
 $installationPath = $toolsDir
 
+# Chocolatey seems to copy the old lib folder in case of upgrade. Uninstall first.
+$zipContentGlob=dir "$($installationPath)/.." "winlogbeat-*.zip.txt"
+$zipContentFile=$zipContentGlob.Name
+$folder = ($zipContentFile -replace ".zip.txt","") + "\\"
+if (($zipContentGlob -ne $null)) {
+    $zipContentFile
+    $zipContents=(get-content $zipContentGlob.FullName) -split [environment]::NewLine
+    for ($i = $zipContents.Length; $i -gt 0; $i--) {
+        $fileInZip = $zipContents[$i]
+        if ($fileInZip -ne $null -and $fileInZip.Trim() -ne '') {
+            $fileToRemove = $fileInZip -replace $folder,""
+            Remove-Item -Path "$fileToRemove" -ErrorAction SilentlyContinue -Recurse -Force
+        }
+    }
+    Remove-Item -Path $zipContentGlob.FullName -ErrorAction SilentlyContinue -Recurse -Force
+}
+
+$folder = if(Get-ProcessorBits 64) { [io.path]::GetFileNameWithoutExtension($url64) } else { [io.path]::GetFileNameWithoutExtension($url) }
+
 $packageArgs = @{
   packageName   = $packageName
   unzipLocation = $installationPath
@@ -17,9 +36,15 @@ $packageArgs = @{
   checksumType  = 'sha512'
   checksum64    = 'ed19c03868cad2975639772405003d3ad2fa217b72161ffac56c65ca16cd2f91aa0f474b5928318255c9b1204d67611052e379dafe9faab2de85c8f4c15de2bd'
   checksumType64= 'sha512'
+  specificFolder = $folder
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
-$pTemp = (dir $installationPath  $packageName*).Name
-Invoke-Expression $(Join-Path $installationPath "$($pTemp)\install-service-$($packageName).ps1")
+# Move everything from the subfolder to the main tools directory
+$subFolder = Join-Path $installationPath (Get-ChildItem $installationPath $folder | ?{ $_.PSIsContainer })
+Get-ChildItem $subFolder -Recurse | ?{$_.PSIsContainer } | Move-Item -Destination $installationPath
+Get-ChildItem $subFolder | ?{$_.PSIsContainer -eq $false } | Move-Item -Destination $installationPath
+Remove-Item "$subFolder"
+
+Invoke-Expression $(Join-Path $installationPath "install-service-$($packageName).ps1")
--- automatic/winlogbeat/tools/chocolateyuninstall.ps1
+++ automatic/winlogbeat/tools/chocolateyuninstall.ps1
@@ -1,11 +1,4 @@
 ﻿$packageName= 'winlogbeat'
-
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$nupkgFile = (Get-Item $(Join-Path $toolsDir "..\*.nupkg")).FullName
-$zipManifest = Get-Content $(Join-Path $toolsDir "..\*.zip.txt")
-$installPath = $zipManifest | select -First 1
-
-$uninstallScript = $zipManifest | where { $_ -imatch 'uninstall.*\.ps1' }
-
+$uninstallScript = Join-Path $toolsDir "uninstall-service-winlogbeat.ps1"
 Invoke-Expression $uninstallScript
-Uninstall-ChocolateyZipPackage $packageName "$($nupkgFile)"
