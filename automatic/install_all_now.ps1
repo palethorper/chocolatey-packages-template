8f32833dac804c8d6a5b07a6c39c190f2f27e0bc
Robert Palethorpe <robert.palethorpe@itaas.dimensiondata.com>
2018-09-25 08:44:29 +1000

    Changing build instal script to allow filter patterns for testing builds

--- automatic/install_all_now.ps1
+++ automatic/install_all_now.ps1
@@ -1,11 +1,14 @@
+param (
+    $patterns = '.*'
+)
 cd $PSScriptRoot
-Get-ChildItem -Recurse -Filter update.ps1 | % {
+Get-ChildItem -Recurse -Filter update.ps1 | ? { $_.DirectoryName -imatch $patterns } | % {
     cd $_.DirectoryName
     Write-Host "Updating package $($_.Name)" -ForegroundColor Yellow
     &".\$($_.Name)"
 
     Write-Host "Now Installing all package in current folder" -ForegroundColor Yellow
     Get-ChildItem -Filter *.nupkg | % {
-       choco.exe install --allow-downgrade -fdvy ".\$($_.Name)"    
+       choco.exe install --allow-downgrade -fdvy ".\$($_.Name)"
     }
 }
